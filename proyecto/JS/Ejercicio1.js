4e0534dfb87cd8cbb302dda863e0ff0c564a54a7
romero23martinez <romeromartinez23@iespabloserrano.com>
2025-01-22 21:29:27 +0100

    Comienzo JS

---
+++ proyecto/JS/Ejercicio1.js
@@ -0,0 +1,17 @@
+function mifuncion(){
+    document.getElementById("p2").innerHTML="modifico el parrafo 2"
+}
+function mifuncion2(){
+    document.getElementById("p2").innerHTML="parrafo2"
+}
+
+function mifuncion3(){
+    document.getElementById("p1").innerHTML="eres feo"
+}
+
+let x, z;
+x= 13;
+z= 12;
+
+let a = x + z;
+
