043febb57008fbe9a7ef52c58e6cb1a39c1ff5f6
Arturo <romeromartinez23@iespabloserrano.com>
2025-02-27 16:12:44 +0100

    carpeta examen

---
+++ proyecto/JS/diaExamen/js/script.js
@@ -0,0 +1,24 @@
+const arrayImagenes = [
+    "./img/1.png",
+    "./img/2.png",
+    "./img/3.png",
+    "./img/4.png",
+    "./img/5.png",
+    "./img/6.png"
+
+];
+
+function cambiar(imagen){
+    let imagenActual = imagen.src.split("/").pop();
+    let indiceActual = arrayImagenes.indexOf("./img/"+ imagenActual);
+
+    let nuevoIndice = (indiceActual + 1) % arrayImagenes.length;
+    imagen.src = arrayImagenes[nuevoIndice];
+}
+
+document.querySelector("#mensaje").innerText = "nuevo texto";
+
+document.querySelectorAll(".btn").forEach(boton =>{
+    boton.style.backgroundColor = "orange";
+    boton.innerText = "botonnn";
+});
